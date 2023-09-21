787c5bf7b0764bc5c4e7ce1ff2612c812cf0e9c4
iNikitaGricenko <VfVristalt@gmail.com>
2023-09-21 16:43:50 +0300

    add package-info and icon

---
+++ src/main/java/io/kestra/plugin/surrealdb/package-info.java
@@ -0,0 +1,9 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using SurrealDB.\n"+
+        "SurrealDB is an end-to-end cloud-native database that reduce development time, and build secure, performant apps quickly and cost-effectively.\n"+
+        "Supports multiple querying languages: SQL, GraphQL, ACID transactions, structured and unstructured data, graph querying, full-text indexing, and geospatial querying.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.surrealdb;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
