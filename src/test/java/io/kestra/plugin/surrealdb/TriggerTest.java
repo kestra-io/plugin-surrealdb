005fd335c3713f71ed2cf8dbf0d0a175f7cf2e29
brian.mulier <bmmulier@hotmail.fr>
2024-06-13 12:30:13 +0200

    fix(tests): use TestsUtils.receive API instead of queue.receive

--- src/test/java/io/kestra/plugin/surrealdb/TriggerTest.java
+++ src/test/java/io/kestra/plugin/surrealdb/TriggerTest.java
@@ -6,6 +6,7 @@ import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
 import io.kestra.core.runners.Worker;
 import io.kestra.core.schedulers.AbstractScheduler;
+import io.kestra.core.utils.TestsUtils;
 import io.kestra.jdbc.runner.JdbcScheduler;
 import io.kestra.core.services.FlowListenersInterface;
 import io.micronaut.context.ApplicationContext;
@@ -13,11 +14,11 @@ import io.kestra.core.junit.annotations.KestraTest;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -52,11 +53,7 @@ public class TriggerTest extends SurrealDBTest {
         Worker worker = new Worker(applicationContext, 8, null);
 
         try (AbstractScheduler scheduler = new JdbcScheduler(this.applicationContext, this.flowListeners)) {
-            AtomicReference<Execution> last = new AtomicReference<>();
-
-            Runnable receive = executionQueue.receive(execution -> {
-                last.set(execution.getLeft());
-
+            Flux<Execution> receive = TestsUtils.receive(executionQueue, execution -> {
                 queueCount.countDown();
                 assertThat(execution.getLeft().getFlowId(), is("surrealdb-listen"));
             });
@@ -68,9 +65,8 @@ public class TriggerTest extends SurrealDBTest {
 
             boolean await = queueCount.await(1, TimeUnit.MINUTES);
             assertThat(await, is(true));
-            receive.run();
 
-            return last.get();
+            return receive.blockLast();
         }
     }
 
