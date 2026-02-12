88f8b8c6189d76c7e6ea9b6df580bee6461b8ec7
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 11:31:05 +0100

    chore: merge package categories attribute

--- src/main/java/io/kestra/plugin/surrealdb/package-info.java
+++ src/main/java/io/kestra/plugin/surrealdb/package-info.java
@@ -1,9 +1,7 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using SurrealDB.\n"+
         "SurrealDB is an end-to-end cloud-native database that reduce development time, and build secure, performant apps quickly and cost-effectively.\n"+
-        "Supports multiple querying languages: SQL, GraphQL, ACID transactions, structured and unstructured data, graph querying, full-text indexing, and geospatial querying.",
-    categories = PluginSubGroup.PluginCategory.DATABASE,
-    categories = {
+        "Supports multiple querying languages: SQL, GraphQL, ACID transactions, structured and unstructured data, graph querying, full-text indexing, and geospatial querying.",categories = {
         PluginSubGroup.PluginCategory.DATA
     }
 )
